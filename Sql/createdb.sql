5f866147ad74added0fa0d9e31a2906e85aa4802
Juan Camba <juan_camba@yahoo.com.ar>
2023-10-27 18:14:53 +0200

    agrego sql

--- Sql/createdb.sql
+++ Sql/createdb.sql
@@ -1,4 +1,5 @@
-﻿use prueba
+﻿create database prueba
+use prueba
 create table campos
 (
 	id int identity(1,1) not null,
@@ -36,8 +37,59 @@ create table valoresDatosEstaticos
     REFERENCES nombresDatosEstaticos(id),
 	
 )
-// https://www.w3schools.com/sql/sql_foreignkey.asp
+create table muestras(
+	id int identity(1,1) not null,
+	idEntidad varchar(255) not null,
+	idTipoMuestra int not null,
+	idCampo int not null,
+	fecha datetime not null,
+	PRIMARY KEY (id),
+	CONSTRAINT FK_entidades_muestras FOREIGN KEY (idEntidad)
+	REFERENCES entidades(id) on delete cascade,
+	constraint FK_tiposMuestras_muestras FOREIGN KEY (idTipoMuestra)
+	REFERENCES tiposMuestras(id),
+	constraint FK_campos_muestras FOREIGN KEY (idCampo)
+	REFERENCES campos(id)
 
+)
+create table nombresVariablesMuestras(
+	id int identity(1,1) not null,
+	nombre varchar(255) not null,
+	idTipoMuestra int not null,
+	PRIMARY KEY (id),
+	CONSTRAINT FK_tiposMuestras_nombresVariablesMuestras FOREIGN KEY (idTipoMuestra)
+	REFERENCES tiposMuestras(id)
+
+)
+create table valoresVariablesMuestras(
+	id int identity(1,1) not null,
+	idNombreVariableMuestra int not null,
+	idMuestra int not null,
+	valor varchar(1024) null,
+	PRIMARY KEY (id),
+	CONSTRAINT FK_nombresVariablesMuestra_valoresVariablesMuestras 
+	FOREIGN KEY (idNombreVariableMuestra) REFERENCES nombresVariablesMuestras(id),
+	CONSTRAINT FK_muestras_valoresVariablesMuestras
+	FOREIGN KEY (idMuestra) REFERENCES muestras(id) on delete cascade
+	
+)
+
+create table valoresReferencia(
+	id int identity(1,1) not null,
+	idNombreVariableMuestra int not null,
+	maximo varchar(255) null,
+	minimo varchar(255) null,
+	PRIMARY KEY (id),
+	CONSTRAINT FK_nombresVariablesMuestra_valoresReferencia 
+	FOREIGN KEY (idNombreVariableMuestra)
+	REFERENCES nombresVariablesMuestras(id) on delete cascade
+
+)
+
+
+/*
+-- https://www.w3schools.com/sql/sql_foreignkey.asp
+*/
 
 
 insert into campos values ('campo')
@@ -49,5 +101,5 @@ insert into valoresdatosestaticos (valor, idEntidad, idNombreDatoEstatico) value
 select * from valoresdatosestaticos
 
 
-drop table valoresdatosestaticos
+
 
---
+++ Sql/pruebas.sql
@@ -0,0 +1,40 @@
+﻿-- generamos un campo, tipo muestra, entidad, nombre dato estatico, valor dato estatico
+insert into campos values ('campo')
+insert into tiposMuestras values ('tiposMuestras')
+insert into Entidades values ('e1', getdate())
+insert into nombresDatosEstaticos values ('var1')
+insert into valoresdatosestaticos (valor, idEntidad, idNombreDatoEstatico) values (1,'e1',1)
+
+-- generamos una muestra
+insert into muestras values ('e1',1,1,getdate())
+insert into nombresVariablesMuestras values ('var1',1)
+insert into nombresVariablesMuestras values ('var2',1)
+insert into valoresVariablesMuestras values (1,1,100)
+insert into valoresVariablesMuestras values (2,1,100)
+insert into valoresReferencia values (1,200,100)
+
+-- comprobaciones
+select * from entidades
+select * from nombresDatosEstaticos
+select * from valoresDatosEstaticos
+
+select * from muestras
+-- variables que tiene una muestra
+select * from nombresVariablesMuestras
+-- valores de las variables de una muestra
+select * from valoresVariablesMuestras
+
+select * from valoresReferencia
+
+-- pruebas
+/*
+1 al borrar una entitdad se borran sus muestras
+2 al borrar una entidad se borran sus valores de datos estaticos
+3 al borrar una entidad se borran sus valores de variables de muestras
+4 al borrar una entidad NO se borran sus nombres de variables de muestras
+5 al borrar una muestras se borran sus valores de variables de muestras
+6 al borrar un nombre de variable no se borran sus valores de variables de muestras
+7 al borrar un nombre de variable, si no tiene muestras se borra su valor de referencia
+*/
+
+
